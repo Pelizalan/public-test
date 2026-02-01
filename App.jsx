41b4f93a343ef166680c8455c45d0bdb5bee942f
Grub <pelikanzalanm@elg-bp.edu.hu>
2026-02-01 18:59:29 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import { Toaster } from '@/components/ui/toaster';
+
+function App() {
+  return (
+    <Router>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+      <Toaster />
+    </Router>
+  );
+}
+
+export default App;
